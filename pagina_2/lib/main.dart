2c06f25fe2e3e12d00acd015e21a38de15106a2e
Carlos Casado <carloscasadovargas@gmail.com>
2022-08-13 23:22:38 -0500

    Pagina_2 register basico funcionando

---
+++ pagina_2/lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String titulo = 'Login - Pagina 1';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: titulo,
+      home: Scaffold(
+        body: RegistroWidget(),
+      ),
+    );
+  }
+}
+
+class RegistroWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _RegistroWidgetState();
+  }
+}
+
+class _RegistroWidgetState extends State<RegistroWidget> {
+  TextEditingController nombreControlador = TextEditingController();
+  TextEditingController emailControlador = TextEditingController();
+  TextEditingController passControlador = TextEditingController();
+  TextEditingController cpassControlador = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(20),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(top: 30),
+              padding: const EdgeInsets.all(20),
+              child: const Text(
+                "Regístrese",
+                style: TextStyle(
+                    color: Colors.purple,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 30),
+              ),
+            ),
+            Container(
+              // Campo para el nombre
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nombreControlador,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Nombre"),
+              ),
+            ),
+            Container(
+              // Campo para el email
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: emailControlador,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Correo electrónico"),
+              ),
+            ),
+            Container(
+              // Campo para la contraseña
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: passControlador,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Contraseña"),
+              ),
+            ),
+            Container(
+              // campo para confirmar la contraseña
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: cpassControlador,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Confirme su contraseña"),
+              ),
+            ),
+            Container(
+                // Boton iniciar sesión
+                height: 50,
+                margin: const EdgeInsets.only(top: 20),
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: ElevatedButton(
+                  child: const Text(
+                    'Registrarse',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    print("Nombre: " + nombreControlador.text);
+                    print("Email: " + emailControlador.text);
+                    print("Contraseña: " + cpassControlador.text);
+                    print("Contraseña: " + passControlador.text);
+                  },
+                  style: ElevatedButton.styleFrom(primary: Colors.purple),
+                )),
+            Row(
+              children: <Widget>[
+                const Text(
+                  '¿Ya tiene una cuenta?',
+                  style: TextStyle(fontSize: 15),
+                ),
+                TextButton(
+                  child: const Text(
+                    'Inicie Sesión',
+                    style: TextStyle(fontSize: 15, color: Colors.purple),
+                  ),
+                  onPressed: () {
+                    // Codigo correspondiente
+                  },
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+          ],
+        ));
+  }
+}
